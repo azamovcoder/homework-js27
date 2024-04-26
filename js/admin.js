c0f42144e9a0d521204ee74dcd8713507e109215
azamovcoder <azamovcoder@gmail.com>
2024-04-26 15:41:27 +0300

    ass

--- js/admin.js
+++ js/admin.js
@@ -1,7 +1,13 @@
-let isLogin = null;
-
+let isLogin = localStorage.getItem("x-auth-token");
+let logOut = document.querySelector(".admin__logout");
 function checkUser() {
   if (!checkUser) {
     window.location.replace("../pages/login.html");
   }
 }
+
+logOut.addEventListener("submit", () => {
+  console.log("ok");
+  localStorage.clear();
+  window.open("../index.html");
+});
